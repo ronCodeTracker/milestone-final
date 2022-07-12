b71ff263884ad47eafd200f06662982f76c7c06c
unknown <2fixelectronics777@gmail.com>
2022-07-11 21:54:20 -0500

    add models2 mod

---
+++ backend/models/index.js
@@ -0,0 +1,9 @@
+
+require('dotenv').config()  //configuration
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
