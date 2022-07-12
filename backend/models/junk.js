778d4625671d2af921c4081aa1fe2d3e2fa58f8c
unknown <2fixelectronics777@gmail.com>
2022-07-11 21:58:37 -0500

    add model3 mod

--- backend/models/junk.js
+++ backend/models/junk.js
@@ -1,3 +1,16 @@
 
 const mongoose = require('mongoose')
 
+
+
+const junkSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: { type: String, default: 'http://placekitten.com/350/350' },
+    price: { type: String, required: false, default: 'none' },
+    receipt: { type: String, default: 'none' },
+    datepurchased: { type: String, default: 'USA' },
+    
+    }
+})
+
+
