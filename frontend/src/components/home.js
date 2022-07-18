30dbf1eb62ce9edcc780e0287ee6c25e7e817742
Hannah Gooding <hgooding6@gmail.com>
2022-07-18 18:10:03 -0500

    added components

---
+++ frontend/src/components/home.js
@@ -0,0 +1,29 @@
+
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Garage from './garage.js';
+import Attic from './attic.js';
+import Basement from './basement.js';
+import Junk from './junk.js';
+
+
+export default function Home () {
+    return(
+        <div className="home">
+            <Container>
+                <Card border="info" >
+                    <Card.Body>
+                        <Card.Text style={{ fontSize: "2.5em" }}>
+                            Junk Around the House
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                    <body>
+                        <Garage />
+                        <Attic />
+                        <Basement />
+                    </body>
+                </Container>
+                </div>
+    )
+}
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,69 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Home from './home.js';
+import Garage from './garage.js';
+import Attic from './attic.js';
+import Basement from './basement.js';
+import Junk from './junk.js';
+import Login from './login.js';
+import Search from './search.js';
+
+
+
+function NavBar() {
+  return (
+    <><Navbar bg="light" expand="lg" fixed="top">
+          <Container fluid>
+              <Navbar.Brand href="/">Junk Around The House</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                      className="me-auto my-2 my-lg-0"
+                      style={{ maxHeight: '100px' }}
+                      navbarScroll
+                  >
+                      <Nav.Link href="/">Home</Nav.Link>
+                      <Nav.Link href="/junk">Junk</Nav.Link>
+                      <NavDropdown title="Rooms" id="navbarScrollingDropdown">
+                          <NavDropdown.Item href="/garage">Garage</NavDropdown.Item>
+                          <NavDropdown.Item href="/attic">
+                              Attic
+                          </NavDropdown.Item>
+
+                          <NavDropdown.Item href="/basement">
+                              Basement
+                          </NavDropdown.Item>
+                      </NavDropdown>
+                      <Nav.Link href="/login">Login</Nav.Link>
+                  </Nav>
+                  <Form className="d-flex">
+                      <Form.Control
+                          type="search"
+                          placeholder="Search"
+                          className="me-2"
+                          aria-label="Search" />
+                      <Button variant="outline-success">Search</Button>
+                  </Form>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar><div className="Display">
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/junk" element={<Junk />} />
+                  <Route path="/garage" element={<Garage />} />
+                  <Route path="/attic" element={<Attic />} />
+                  <Route path="/basement" element={<Basement />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/search" element={<Search />} />
+              </Routes>
+          </div></>
+  )
+}
+
+export default NavBar;
